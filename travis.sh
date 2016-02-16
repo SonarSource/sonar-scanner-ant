dd00127f313b4c0eb414735507ee2fa6c3e11db1
Julien HENRY <julien.henry@sonarsource.com>
2016-02-16 09:39:35 +0100

    Use repox

--- travis.sh
+++ travis.sh
@@ -2,18 +2,17 @@
 
 set -euo pipefail
 
-function installTravisTools {
+function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v25 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+configureTravis
 
 function strongEcho {
   echo ""
   echo "================ $1 ================="
 }
-
-installTravisTools
 
 case "$TARGET" in
 
@@ -21,12 +20,13 @@ CI)
   if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     strongEcho 'Build and analyze commit in master'
     export MAVEN_OPTS="-Xmx1G -Xms128m"
+    git fetch --unshallow || true
     mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
       -Pcoverage-per-test -Dmaven.test.redirectTestOutputToFile=false -B -e -V \
       -Dsonar.host.url=$SONAR_HOST_URL \
       -Dsonar.login=$SONAR_TOKEN
 
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
     strongEcho 'Build and analyze pull request'
     mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
       -Pcoverage-per-test -Dmaven.test.redirectTestOutputToFile=false -B -e -V \
@@ -50,11 +50,6 @@ CI)
 IT)
   start_xvfb
   mvn install -T2 -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  if [ "${SQ_VERSION}" == "DEV" ]
-  then
-    build_snapshot "SonarSource/sonarqube"
-  fi
 
   cd its
   mvn -Dsonar.runtimeVersion="${SQ_VERSION}" -Dmaven.test.redirectTestOutputToFile=false verify
